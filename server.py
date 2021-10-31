3804418de58c1592cac269ded570f6f1269ea214
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-30 22:35:09 -0600

    Add Counts

--- server.py
+++ server.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request
 app = Flask(__name__)
+app.secret_key = "skittles"
+
+
 
 @app.route('/')
 def hello():
+    if 'count' in session:
+        session['count'] = session['count'] +1
+    else:
+        session['count'] = 0
     return render_template("index.html")
+
+@app.route('/add')
+def add_count():
+    session['count'] = session['count'] + 1
+    return redirect('/')
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect("/",)
+
+@app.route('/user_add', methods = ["POST"])
+def user_input():
+    session['count'] += int(request.form['input']) - 1
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
